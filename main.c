c8ec42b3d79da8d44b6a6c320ace0b3ef16d1266
Fred <fred@gmail.com>
2016-10-13 16:15:22 +0300

    change 'hi there' to 'Hi'.

--- main.c
+++ main.c
@@ -4,6 +4,6 @@ int main(int argc, char **argv)
 {
 char first[255], last[255];
 askname(first, last);
-printf("Hi there!, %s %s!\n", first, last);
+printf("Hi!, %s %s!\n", first, last);
 return 0;
 }
